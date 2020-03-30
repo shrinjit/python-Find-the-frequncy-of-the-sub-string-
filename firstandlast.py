1f04a1102802e5a81063e5785fa1cce28892a08a
Satyajit <satyajit.rout@dhira.io>
2020-03-30 12:06:44 +0530

    second commit

---
+++ firstandlast.py
@@ -0,0 +1,18 @@
+t = int(input())
+while t>0:
+    input()
+    arr = list(map(int,input().split()))
+    x = int(input())
+    first,last = -1,-1
+    for i in range(len(arr)):
+        if arr[i] == x:
+            if first == -1:
+                first,last=i,i
+            else:
+                last = i
+    if first == -1:
+        print(first)
+    else:
+        print(first,last)
+    t -= 1
+© 2020 GitHub, Inc.
